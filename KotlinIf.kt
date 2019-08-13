3dbc5903725529525069b3b08ab1e259503472e8
Sagar Karambelkar <53513296+Mrpool96@users.noreply.github.com>
2019-08-13 09:34:22 +0530

    Add files via upload
    This the simple Kotlin program using if expression

---
+++ KotlinIf.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val read= Scanner(System.`in`)
+    // if expression
+        println("Enter the Even Number")
+        var num=read.nextInt()
+    if (num%2 == 0)
+    println("Out Of If statement")
+}
